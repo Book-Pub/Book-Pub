caa2d694840bcc4159b4923c5820252913a85ad9
marc-bd <marcossbecker@gmail.com>
2022-09-07 13:49:29 -0300

    FEAT/FIX: created Update Ebook and deleted duplicated ebooks folders

---
+++ src/controller/ebooks/updateEbook.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { rmSync } from "fs";
+import updateEbookService from "../../services/ebooks/updateEbooks.service";
+
+const updateEbookController = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data = req.body;
+
+  const updatedEbook = await updateEbookService(id, data);
+
+  return res.status(200).json({
+    message: "Ebook Updated",
+    updatedEbook,
+  });
+};
+
+export default updateEbookController;
--- src/interfaces/ebooks.interface.ts
+++ src/interfaces/ebooks.interface.ts
@@ -13,3 +13,19 @@ export interface IBookRequest {
   country: string;
   isbn: string;
 }
+
+export interface IbookUpdateRequest {
+  author?: string;
+  name?: string;
+  category?: string;
+  dateRelease?: string;
+  bookCover?: string;
+  value?: number;
+  description?: string;
+  publishingCompany?: string;
+  language?: string;
+  editionNumber?: string;
+  numberPages?: string;
+  country?: string;
+  isbn?: string;
+}
