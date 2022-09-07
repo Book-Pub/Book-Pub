9b030db46509ebada409d99284bde8d66b47c916
Marco Arruda <marcoantonioarrudadasilva@gmail.com>
2022-09-07 11:54:34 -0300

    FIX: Removing isactive key from IUserRequest, FEAT: Schema login

--- src/routes/Session/sessionRoutes.ts
+++ src/routes/Session/sessionRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 
+import { handleSchemaLogin } from "../../middlewares/schemas/handleSchemaLogin.middleware";
+import { loginRequestSchema } from "../../schemas/login.schema";
+
 const sessionRoutes = Router();
 
-sessionRoutes.post(""); // Login
+sessionRoutes.post("", handleSchemaLogin(loginRequestSchema)); // Login
 
 export default sessionRoutes;
