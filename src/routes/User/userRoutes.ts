7ebdf6fd9be716503cc10ca2fd0a724db5f60017
Marco Arruda <marcoantonioarrudadasilva@gmail.com>
2022-09-07 11:28:05 -0300

    FEAT: IMPLEMENTED USER SCHEME

--- src/routes/User/userRoutes.ts
+++ src/routes/User/userRoutes.ts
@@ -2,9 +2,12 @@ import { Router } from "express";
 import adminAuthMiddleware from "../../middlewares/adminVerification.middleware";
 import handleAuthMiddleware from "../../middlewares/authentication.middleware";
 
+import { handleSchemaUserCreate } from "../../middlewares/schemas/handleSchemaUserCreate.middleware";
+import { userRequestSchema } from "../../schemas/userCreate.schema";
+
 const userRoutes = Router();
 
-userRoutes.post(""); //cadastro de usuario
+userRoutes.post("", handleSchemaUserCreate(userRequestSchema)); //cadastro de usuario
 userRoutes.get("", handleAuthMiddleware, adminAuthMiddleware); //listar todos os usuarios - SÓ ADMIN
 userRoutes.delete("/:id", handleAuthMiddleware, adminAuthMiddleware); // deletar usuario - softdelete - SÓ ADMIN
 
