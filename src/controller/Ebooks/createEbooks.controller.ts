d09c39eefd61b391b4ed4de1de06776d75159d1a
marc-bd <marcossbecker@gmail.com>
2022-09-07 12:14:02 -0300

    FEAT: created Interface, service and controller to create Ebooks

---
+++ src/controller/Ebooks/createEbooks.controller.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+import { IBookRequest } from "../../interfaces/ebooks.interface";
+import createEbooksService from "../../services/ebooks/createEbooks.service";
+
+const createEbooksController = async (req: Request, res: Response) => {
+  const {
+    author,
+    bookCover,
+    category,
+    country,
+    dateRelease,
+    description,
+    editionNumber,
+    isbn,
+    language,
+    name,
+    numberPages,
+    publishingCompany,
+    value,
+  }: IBookRequest = req.body;
+
+  const eBook = await createEbooksService({
+    author,
+    bookCover,
+    category,
+    country,
+    dateRelease,
+    description,
+    editionNumber,
+    isbn,
+    language,
+    name,
+    numberPages,
+    publishingCompany,
+    value,
+  });
+
+  return res.status(201).json({
+    eBook,
+  });
+};
+
+export default createEbooksController;
---
+++ src/interfaces/ebooks.interface.ts
@@ -0,0 +1,15 @@
+export interface IBookRequest {
+  author: string;
+  name: string;
+  category: string;
+  dateRelease: string;
+  bookCover: string;
+  value: number;
+  description: string;
+  publishingCompany: string;
+  language: string;
+  editionNumber: string;
+  numberPages: string;
+  country: string;
+  isbn: string;
+}
