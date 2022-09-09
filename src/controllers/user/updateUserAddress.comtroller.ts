2d7a76f5a7bcf167cde468583aaea0035ec42755
TheHerran <alexandreherran@outlook.com>
2022-09-08 23:00:29 -0300

    FEAT: update address controller and service, routes

---
+++ src/controllers/user/updateUserAddress.comtroller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import updateUserAddressService from "../../services/user/updateUserAddress.service";
+
+const updateUserAddressController = async (req: Request, res: Response) => {
+    const { id } = req.params
+    const { streetName, district, number, zipCode, city, state } = req.body
+    const updated = await updateUserAddressService(id, { streetName, district, number, zipCode, city, state })
+
+    return res.status(200).json(updated)
+}
+export default updateUserAddressController
